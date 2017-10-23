679fc2223bf933cf1d28ef52f9a6588e9dcfca95
Carlos Atila Abreu <carlos.abreu@fortaleza.ce.gov.br>
2017-10-23 17:17:54 -0300

    Base project with entities

---
+++ src/main/java/com/infonatum/eventmanagement/EventmanagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.infonatum.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmanagementApiApplication.class, args);
+	}
+}
