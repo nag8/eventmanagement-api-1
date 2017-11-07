efcddbb02bd7975db309cf6f65034c2f8eca627b
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-06 23:17:28 -0300

    Adding and configuring EntityScan annotation

--- src/main/java/com/infonatum/eventmanagement/EventmanagementApiApplication.java
+++ src/main/java/com/infonatum/eventmanagement/EventmanagementApiApplication.java
@@ -2,8 +2,11 @@ package com.infonatum.eventmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses= {EventmanagementApiApplication.class,Jsr310Converters.class})
 public class EventmanagementApiApplication {
 
 	public static void main(String[] args) {
