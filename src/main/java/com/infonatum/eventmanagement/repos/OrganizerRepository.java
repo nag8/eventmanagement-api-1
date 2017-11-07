a54f25eaab38578c11bbe74aae1902413ddfad62
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-06 23:53:52 -0300

    Fixing organizer repostory

--- src/main/java/com/infonatum/eventmanagement/repos/OrganizerRepository.java
+++ src/main/java/com/infonatum/eventmanagement/repos/OrganizerRepository.java
@@ -2,8 +2,8 @@ package com.infonatum.eventmanagement.repos;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.infonatum.eventmanagement.entities.Event;
+import com.infonatum.eventmanagement.entities.Organizer;
 
-public interface OrganizerRepository extends CrudRepository<Event, Long>{
+public interface OrganizerRepository extends CrudRepository<Organizer, Long>{
 
 }
