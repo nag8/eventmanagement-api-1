c27d63d29e32c0c3d89d577bf46947d773c5733f
Carlos Atila Abreu <carlos.abreu@fortaleza.ce.gov.br>
2017-11-14 09:03:28 -0300

    Adding findByPostalCode method

--- src/main/java/com/infonatum/eventmanagement/repos/VenueRepository.java
+++ src/main/java/com/infonatum/eventmanagement/repos/VenueRepository.java
@@ -1,10 +1,14 @@
 package com.infonatum.eventmanagement.repos;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-import com.infonatum.eventmanagement.entities.Event;
 import com.infonatum.eventmanagement.entities.Venue;
 
 public interface VenueRepository extends PagingAndSortingRepository<Venue, Long>{
+	
+	Page<Venue> findByPostalCode(@Param("postalCode") String postalCode, Pageable pageable);
 
 }
