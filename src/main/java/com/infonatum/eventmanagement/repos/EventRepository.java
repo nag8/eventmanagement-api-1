789710f9167bbeea34e747e9f34fe299bf22b65a
Carlos Atila Abreu <carlos.abreu@fortaleza.ce.gov.br>
2017-11-14 09:02:35 -0300

    Adding findByName and findByNameAndZoneId methods

--- src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
@@ -1,9 +1,17 @@
 package com.infonatum.eventmanagement.repos;
 
+
+import java.time.ZoneId;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.infonatum.eventmanagement.entities.Event;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long>{
-
+	Page<Event> findByName(@Param("name") String name, Pageable pageable);
+	
+	Page<Event> findByNameAndZoneId(@Param("name")String name,@Param("zoneI	d") ZoneId zoneId, Pageable pageable);
 }
