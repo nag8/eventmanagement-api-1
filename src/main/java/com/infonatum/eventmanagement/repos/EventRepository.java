e96167096ff8e97a3c7f2fbb756b587900b1973b
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-15 08:49:24 -0300

    Setting Java method security to delete method on Event Repository

--- src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.infonatum.eventmanagement.entities.Event;
 import com.infonatum.eventmanagement.entities.projections.PartialEventProjection;
@@ -19,4 +20,7 @@ public interface EventRepository extends PagingAndSortingRepository<Event, Long>
 	
 	Page<Event> findByNameAndZoneId(@Param("name")String name,@Param("zoneI	d") ZoneId zoneId, Pageable pageable);
 
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	void delete(Long id);
 }
