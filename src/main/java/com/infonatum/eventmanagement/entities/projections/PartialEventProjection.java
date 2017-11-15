b7d3c123176668c2df4a8e4580301dd860ef5dd1
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-15 06:37:46 -0300

    Using projections to Event entity

---
+++ src/main/java/com/infonatum/eventmanagement/entities/projections/PartialEventProjection.java
@@ -0,0 +1,14 @@
+package com.infonatum.eventmanagement.entities.projections;
+
+import java.time.ZonedDateTime;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import com.infonatum.eventmanagement.entities.Event;
+
+@Projection(name="partial",types= {Event.class})
+public interface PartialEventProjection {
+	String getName();
+	ZonedDateTime getStartTime();
+	ZonedDateTime getEndTime();
+}
