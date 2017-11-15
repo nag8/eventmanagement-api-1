f624e2e85f0a1755a4332aa44fc507d4b50a069f
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-15 06:55:28 -0300

    Setting EventPartialProject properties as default properties in EventRepository

--- src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Pageable;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.infonatum.eventmanagement.entities.Event;
+import com.infonatum.eventmanagement.entities.projections.PartialEventProjection;
 
+@RepositoryRestResource(excerptProjection=PartialEventProjection.class)
 public interface EventRepository extends PagingAndSortingRepository<Event, Long>{
 
 	Page<Event> findByName(@Param("name") String name, Pageable pageable);
