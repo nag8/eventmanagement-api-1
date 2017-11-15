70b09759f9a3ad60aa87317df3f2faabcadad6fc
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-15 06:23:19 -0300

    Setting started property as first property in json

--- src/main/java/com/infonatum/eventmanagement/entities/Event.java
+++ src/main/java/com/infonatum/eventmanagement/entities/Event.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"resourceId"})
+@JsonPropertyOrder({"started"})
 @Entity
 public class Event extends AbstractEntity {
 
