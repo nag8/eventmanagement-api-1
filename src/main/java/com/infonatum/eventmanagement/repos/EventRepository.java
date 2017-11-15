468e8b86519fe584772a8a9e2a6c3028f2752e4d
Carlos Átila Abreu <carlosatilaabreu@gmail.com>
2017-11-15 06:24:00 -0300

    Adding findByName method

--- src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
+++ src/main/java/com/infonatum/eventmanagement/repos/EventRepository.java
@@ -1,9 +1,12 @@
 package com.infonatum.eventmanagement.repos;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.infonatum.eventmanagement.entities.Event;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long>{
-
+	List<Event> findByName(@Param("name") String name);
 }
